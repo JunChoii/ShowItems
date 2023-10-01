d026d3961e70f6166eb9a3e345b85c7e4f811fa9
Jun <junability@gmail.com>
2023-10-01 08:40:02 -0700

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+// Check line 14
+
+import { Config } from "drizzle-kit"
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.local',
+});
+
+export default {
+  schema: "./src/db/schema/*",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.MIGRATION_DATABASE_URL!, 
+  },
+  out: "./drizzle",
+} satisfies Config
